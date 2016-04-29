44607764faf49c047e119975a8ab07f445922d6d
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-04-29 14:39:07 +0100

    Test to see if a tile can move.

--- SlidePuzzle/puzzle.py
+++ SlidePuzzle/puzzle.py
@@ -6,7 +6,28 @@ message = "WELCOME TO SLIDING PUZZLES!"
 class SlidePuzzle:
 
     def __init__(self):
-        pass
+        self._board = {}
+        for y in range(1, 4):
+            for x in range(1, 4):
+                self._board[(x, y)] = (y - 1) * 3 + x
+        self._board[(3, 3)] = 0
 
     def __str__(self):
-        return "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 |   |"
+        board = ""
+        for y in range(1, 4):
+            board += "| "
+            for x in range(1, 4):
+                if self._board[(x, y)] == 0:
+                    board += "  | "
+                else:
+                    board += str(self._board[(x, y)]) + " | "
+            board = board.rstrip(' ')
+            board += '\n'
+        board = board.rstrip('\n')
+        return board
+
+    def can_move(self, param):
+        xy = [key for key, value in self._board.items() if value == param][0]
+        zero = [key for key, value in self._board.items() if value == 0][0]
+        (zx, zy) = zero
+        return xy in {(zx - 1, zy), (zx, zy - 1), (zx + 1, zy), (zx, zy + 1)}
--- SlidePuzzle/tests/test_puzzle.py
+++ SlidePuzzle/tests/test_puzzle.py
@@ -14,6 +14,17 @@ class PuzzleTest(unittest.TestCase):
         initial_board = "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 |   |"
         self.assertEqual(str(sp), initial_board)
 
+    def test_can_move(self):
+        sp = puzzle.SlidePuzzle()
+        self.assertEqual(sp.can_move(1), False)
+        self.assertEqual(sp.can_move(2), False)
+        self.assertEqual(sp.can_move(3), False)
+        self.assertEqual(sp.can_move(4), False)
+        self.assertEqual(sp.can_move(5), False)
+        self.assertEqual(sp.can_move(6), True)
+        self.assertEqual(sp.can_move(7), False)
+        self.assertEqual(sp.can_move(8), True)
+
 
 def all_tests():
     return unittest.TestSuite([
